5a6aef50cf0e34fbe8b29c9a37314ae5d1fdc0cf
Lars Strömberg <lars@dsve.se>
2024-04-02 15:44:19 +0000

    Add CodeValidityTimer.java

---
+++ src/main/java/se/dsve/timer/CodeValidityTimer.java
@@ -0,0 +1,19 @@
+package se.dsve.timer;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class CodeValidityTimer {
+
+    LocalDateTime codeGenerationTime;
+    private static final long VALIDITY_PERIOD_SECONDS = 0;
+
+    public void startTimer() {
+        // TODO: Skriv din kod här
+    }
+
+    public boolean isTimeValid() {
+        // TODO: Skriv din kod här
+        return false;
+    }
+}
