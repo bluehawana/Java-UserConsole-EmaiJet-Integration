c661a8f72339962830d2ac59d289487c723ca6b8
Lars Strömberg <lars@dsve.se>
2024-04-02 15:44:20 +0000

    Add PasswordGenerator.java

---
+++ src/main/java/se/dsve/passwordGenerator/PasswordGenerator.java
@@ -0,0 +1,16 @@
+package se.dsve.passwordGenerator;
+
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+
+    static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final int PASSWORD_LENGTH = -1;
+    private static final SecureRandom random = new SecureRandom();
+
+    public static String generatePassword() {
+        // TODO: Skriv din kod här
+        return "";
+    }
+}
+
