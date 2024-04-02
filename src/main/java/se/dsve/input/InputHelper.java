f4b961ec16add57ed15078c1cbe3684f037fe03c
Lars Strömberg <lars@dsve.se>
2024-04-02 15:44:19 +0000

    Add InputHelper.java

---
+++ src/main/java/se/dsve/input/InputHelper.java
@@ -0,0 +1,38 @@
+package se.dsve.input;
+
+import java.util.Scanner;
+
+public class InputHelper {
+	private Scanner scanner;
+
+	public InputHelper() {
+		scanner = new Scanner(System.in);
+	}
+
+	public String readString() {
+//		System.out.print("Ange en sträng: ");
+		return scanner.nextLine();
+	}
+
+	public int readInt() {
+		int number = 0;
+		boolean valid = false;
+		while (! valid) {
+//			System.out.print("Ange ett heltal: ");
+			if (scanner.hasNextInt()) {
+				number = scanner.nextInt();
+				valid = true;
+			} else {
+				System.out.println("Det är inte ett giltigt heltal. Försök igen.");
+			}
+			scanner.nextLine();
+		}
+		return number;
+	}
+
+	public void close() {
+		if (scanner != null) {
+			scanner.close();
+		}
+	}
+}
